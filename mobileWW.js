3cbd53ce2beb0f503915cd431795b6c7ef16c0ff
samwoodburnn <137844265+samwoodburnn@users.noreply.github.com>
2023-08-11 18:27:56 -0400

    Update mobileWW.js

--- mobileWW.js
+++ mobileWW.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('#mobile-menu');
 const menulinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-//display menu
+// Display menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menulinks.classList.toggle('active');
@@ -10,16 +10,20 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-//show active menu when scrolling
+// Hide menu after clicking a link
+menulinks.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menulinks.classList.remove('active');
+});
+
+// Show active menu when scrolling
 const highlightMenu = () => {
-    const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('.home-page');
     const pushMenu = document.querySelector('.push-page');
-    const pullMenu = document.querySelector('.pull-page');    
-    const legsMenu = document.querySelector('.legs-page');  
-    const absMenu = document.querySelector('.abs-page'); 
-    let scollPos = windor.scrollY;
+    const pullMenu = document.querySelector('.pull-page');
+    const legsMenu = document.querySelector('.legs-page');
+    const absMenu = document.querySelector('.abs-page');
+    const gymMusicMenu = document.querySelector('.gymMusic-page');
+    let scrollPos = window.scrollY;
+};
 
-    //adds the highlight class to my menu items
-  //  if(window.innerWidth > 960 )
-}
